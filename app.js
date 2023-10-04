1518e2e00787da46046824172cd65b51140f1bba
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-04 17:14:58 +0200

    Done

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
-const karticky = document.querySelectorAll('.karticka')
+const karticky = document.querySelectorAll('.karticka');
+// první karta v poli má nastavenou třídu
 
-karticky[0].classList.remove('otocena')
+const addClass = (e) => {
+  const karticka = e.target;
+  karticka.classList.toggle('otocena');
+
+  if (!karticka.classList.contains('otocena')) {
+    if (
+      karticka.querySelector('img').src === karticka.querySelector('img').src
+    ) {
+    }
+    setTimeout(() => {
+      return karticka.classList.add('otocena');
+    }, 1000);
+  }
+};
+
+karticky.forEach((karticka) => {
+  karticka.addEventListener('click', addClass);
+});
