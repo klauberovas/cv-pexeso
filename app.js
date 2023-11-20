aaab0f4782aed6d93f626ecac79d37ae20159a1a
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-20 16:23:06 +0100

    poznámky

--- app.js
+++ app.js
@@ -11,28 +11,35 @@ const otocKartu = (e) => {
   if (!e.target.classList.contains('otocena') || e.disabled === true) {
     return;
   }
-  //otoč kartu
+  //nebo otoč kartu
   e.target.classList.remove('otocena');
   otoceneKarticky.push(e);
 
+  //pole otocených karet obsahuje 2 znaky
   if (otoceneKarticky.length === 2) {
     const [karticka1, karticka2] = otoceneKarticky;
+    //1. vybere z karet zdroje
     const obrazek1 = karticka1.target.querySelector('img').src;
     const obrazek2 = karticka2.target.querySelector('img').src;
-
+    //2. porovná zdroje
     if (obrazek1 === obrazek2) {
+      //když se rovnají zablokuje je, aby se neotočily
       karticka1.disabled = true;
       karticka2.disabled = true;
-
+      //vyprázdní pole
       otoceneKarticky = [];
+      //pushne je do zablokovaných karet
       zablokovaneKarticky.push(karticka1);
       zablokovaneKarticky.push(karticka2);
 
+      //Když pole zablokovaných karet je dlouhé 36 znaků, spustí se časovač HRA SKONČILA
       if (zablokovaneKarticky.length === 36) {
         setTimeout(() => {
           alert('Hra skončila.');
         }, 1000);
       }
+
+      // Nebo pokud nevyšla ani jedna podmínka, otočí je zpět a vyprádní pole otočených karet
     } else {
       setTimeout(() => {
         karticka1.target.classList.add('otocena');
